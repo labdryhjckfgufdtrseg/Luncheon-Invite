6aa89a5737a086817a8f3623906144b802c9e73a
labdryhjckfgufdtrseg <1chilli2milli@gmail.com>
2026-07-05 18:53:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,31 @@
 const seal = document.getElementById("seal");
 const envelope = document.getElementById("envelope");
-const app = document.getElementById("app");
-const music = document.getElementById("bgMusic");
+const mainContent = document.getElementById("mainContent");
+const music = document.getElementById("music");
 
 seal.addEventListener("click", () => {
-  envelope.classList.add("open");
+    envelope.classList.add("open");
 
-  setTimeout(()=>{
-    document.getElementById("envelopeSection").style.display="none";
-    app.classList.remove("hidden");
-    music.play();
-  },1500);
+    setTimeout(() => {
+        document.querySelector(".envelope-section").style.display = "none";
+        mainContent.style.display = "block";
+        music.play();
+    }, 1500);
 });
-
-/* SCENE CONTROL */
-let current = 0;
-const scenes = document.querySelectorAll(".scene");
-
-/* TAP OR SWIPE DOWN */
-document.addEventListener("click", nextScene);
-
-let startY=0;
-document.addEventListener("touchstart", e=>{
-  startY = e.touches[0].clientY;
-});
-
-document.addEventListener("touchend", e=>{
-  let endY = e.changedTouches[0].clientY;
-  if(startY - endY > 50){
-    nextScene();
-  }
-});
-
-function nextScene(){
-  scenes[current].classList.remove("active");
-  current++;
-  if(scenes[current]){
-    scenes[current].classList.add("active");
-  }
-}
-
-/* CARD REVEAL */
-function reveal(card){
-  card.classList.toggle("open");
-}
 
 /* COUNTDOWN */
 const countdown = document.getElementById("countdown");
 const eventDate = new Date("July 21, 2026 10:00:00").getTime();
 
-setInterval(()=>{
-  const now = new Date().getTime();
-  const gap = eventDate - now;
+setInterval(() => {
+    const now = new Date().getTime();
+    const gap = eventDate - now;
 
-  const d = Math.floor(gap/(1000*60*60*24));
-  const h = Math.floor((gap/(1000*60*60))%24);
-  const m = Math.floor((gap/(1000*60))%60);
-  const s = Math.floor((gap/1000)%60);
+    const d = Math.floor(gap / (1000*60*60*24));
+    const h = Math.floor((gap % (1000*60*60*24)) / (1000*60*60));
+    const m = Math.floor((gap % (1000*60*60)) / (1000*60));
+    const s = Math.floor((gap % (1000*60)) / 1000);
 
-  countdown.innerHTML = `${d}d ${h}h ${m}m ${s}s`;
-},1000);
+    countdown.innerHTML = `${d}d ${h}h ${m}m ${s}s`;
+
+}, 1000);
